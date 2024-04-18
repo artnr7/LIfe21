1b89b3db49139776592f1e56e3f85cfedcb3a2eb
Artur Nigmatullin <artnr7@gmail.com>
2024-04-18 18:11:06 +0300

    ver with structures

--- main.c
+++ main.c
@@ -9,6 +9,17 @@ int main()
     setlocale(LC_ALL, "Rus"); // поддержка кириллицы в терминале
     system("chcp 65001");     // переключаем шрифт для отображения кириллицы
 
+    /**/
+    int y = 24;
+    int x = 79;
+    int c; // input
+    struct cell
+    {
+        int y;
+        int x;
+    };
+
+    /**/                                    //
     initscr();                              //
     keypad(stdscr, TRUE);                   //
     curs_set(0);                            // hide cursor
@@ -21,9 +32,23 @@ int main()
     init_pair(1, COLOR_WHITE, COLOR_BLACK); // режим цветов
     bkgd(COLOR_PAIR(1));                    // цвет фона, режим 1
 
-    while (1)
+    struct cell first;
+    struct cell second;
+    struct cell third;
+    while ((c = getch()) != 27)
     {
-        getch();
+        if (c == 32)
+        {
+            first.y = y / 2;
+            first.x = x / 2;
+            mvaddch(first.y, first.x, 'O');
+            second.y = first.y + 1;
+            second.x = x / 2;
+            mvaddch(second.y, first.x, 'O');
+            third.y = first.y + 2;
+            third.x = x / 2;
+            mvaddch(third.y, first.x, 'O');
+        }
     }
     endwin();
 
