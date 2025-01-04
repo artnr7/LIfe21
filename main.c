56e6e3c6c0324d348f2b35d90e5040d89493b9d5
Artur Nigmatullin <artnr7@gmail.com>
2025-01-05 01:06:49 +0300

    05.01.2025 01:06

--- main.c
+++ main.c
@@ -3,53 +3,48 @@
 #include <pdcurses.h> //console
 #include <unistd.h>   //usleep
 
+void change(char *arr[], int *prty, char *orr, char *err)
+{
+    if (*prty == 1)
+    {
+        arr = err;
+        *prty = 2;
+    }
+    else if (*prty == 2)
+    {
+        arr = orr;
+        *prty = 1;
+    }
+}
+
 int main()
 {
     /***PRE-GAME*******************************************************************************/
 
     /**VARIABLES**/
-    /*Main Var*/
+    /*Main Vars*/
     const int ysz = 25, xsz = 80;                 // size of playing field
     const int yarrsz = ysz + 2, xarrsz = xsz + 2; // arrays size
     int c;                                        // input var for ASCII
     int bb = 0;                                   // counter for number of live cells nearby
-    char sch = ' ';                               // space char
+    char sch = '.';                               // space char
 
     /*Arrays Of Chars*/
-    char marr[yarrsz][xarrsz];  // main array
-    char bfarr[yarrsz][xarrsz]; // buffer array
-    struct crds                 // struct for working with 2-dimensions var
+    char orr[yarrsz][xarrsz]; // odd array
+    char err[yarrsz][xarrsz]; // even array
+    char(*arr)[yarrsz] = orr; // реализация с безымянными периодическими массивами
+    int prty = 1;             // четность - нечетность
+    struct crds               // struct for working with 2-dimensions var
     {
         int y, x;
     };
 
-    /**Files**/
+    /**FILES**/
+    /*File vars*/
     FILE *act_file;
     int nmb_of_fls = 3; // number of files with coordinates
     int choice;         // var for working with files
     int nmb_of_cls;
-
-    /**PDCURSES**/
-    /*Init Screen*/
-    initscr();   // init main screen(console)
-    curs_set(0); // hide cursor
-    noecho();    // no enter effect
-
-    /*Color Mode*/
-    start_color();                          // init color mode
-    init_pair(1, COLOR_GREEN, COLOR_BLACK); // create color pair
-    bkgd(COLOR_PAIR(1));                    // color background pair
-
-    /***GAME**************************************************************************************/
-
-    /*Entering Arrays Of Chars*/
-    for (int i = 0; i < yarrsz; i++)
-    {
-        for (int j = 0; j < xarrsz; j++)
-        {
-            marr[i][j] = sch;
-        }
-    }
 
     /*Init cells*/
     while (true)
@@ -116,12 +111,34 @@ int main()
     }
     fclose(act_file);
 
+    /**PDCURSES**/
+    /*Init Screen*/
+    initscr();   // init main screen(console)
+    curs_set(0); // hide cursor
+    noecho();    // no enter effect
+
+    /*Color Mode*/
+    start_color();                          // init color mode
+    init_pair(1, COLOR_GREEN, COLOR_BLACK); // create color pair
+    bkgd(COLOR_PAIR(1));                    // color background pair
+
+    /*Entering Arrays Of Chars*/
+    for (int i = 0; i < yarrsz; i++)
+    {
+        for (int j = 0; j < xarrsz; j++)
+        {
+            orr[i][j] = sch;
+            err[i][j] = sch;
+        }
+    }
+
     /*Cords of cells*/
     for (int i = 0; i < nmb_of_cls; i++)
     {
-        marr[cell[i].y][cell[i].x] = 'O';
+        arr[cell[i].y][cell[i].x] = 'O';
     }
 
+    /***GAME*************************************************************************************************/
     /***MAIN GAME CYCLE**************************************************************************************/
     do
     {
@@ -129,100 +146,89 @@ int main()
         /*entering array edges*/
         for (int i = 1; i < xarrsz - 1; i++)
         {
-            marr[0][i] = marr[yarrsz - 2][i];
-            bfarr[0][i] = bfarr[yarrsz - 2][i];
-            marr[yarrsz - 1][i] = marr[1][i];
-            bfarr[yarrsz - 1][i] = bfarr[1][i];
+            arr[0][i] = arr[yarrsz - 2][i];
+            arr[yarrsz - 1][i] = arr[1][i];
         }
         for (int i = 1; i < yarrsz - 1; i++)
         {
-            marr[i][0] = marr[i][xarrsz - 2];
-            bfarr[i][0] = bfarr[i][xarrsz - 2];
-            marr[i][xarrsz - 1] = marr[i][1];
-            bfarr[i][xarrsz - 1] = bfarr[i][1];
+            arr[i][0] = arr[i][xarrsz - 2];
+            arr[i][xarrsz - 1] = arr[i][1];
         }
-        marr[0][0] = marr[yarrsz - 2][xarrsz - 2];
-        bfarr[0][0] = bfarr[yarrsz - 2][xarrsz - 2];
-        marr[0][xarrsz - 1] = marr[yarrsz - 2][1];
-        bfarr[0][xarrsz - 1] = bfarr[yarrsz - 2][1];
-        marr[yarrsz - 1][0] = marr[1][xarrsz - 2];
-        bfarr[yarrsz - 1][0] = bfarr[1][xarrsz - 2];
-        marr[yarrsz - 1][xarrsz - 1] = marr[1][1];
-        bfarr[yarrsz - 1][xarrsz - 1] = bfarr[1][1];
+        arr[0][0] = arr[yarrsz - 2][xarrsz - 2];
+        arr[0][xarrsz - 1] = arr[yarrsz - 2][1];
+        arr[yarrsz - 1][0] = arr[1][xarrsz - 2];
+        arr[yarrsz - 1][xarrsz - 1] = arr[1][1];
 
         /*The Main Cycle Of Searching For Places For New Life*/
         for (int i = 1; i < yarrsz - 1; i++)
         {
             for (int j = 1; j < xarrsz - 1; j++)
             {
-                mvprintw(i - 1, j - 1, "%c", marr[i][j]);
-                if (marr[i - 1][j - 1] == 'O') /*top left*/
+                mvprintw(i - 1, j - 1, "%c", arr[i][j]);
+                if (arr[i - 1][j - 1] == 'O') /*top left*/
                 {
                     bb++;
                 }
-                if (marr[i - 1][j] == 'O') /*top center*/
+                if (arr[i - 1][j] == 'O') /*top center*/
                 {
                     bb++;
                 }
-                if (marr[i - 1][j + 1] == 'O') /*top right*/
+                if (arr[i - 1][j + 1] == 'O') /*top right*/
                 {
                     bb++;
                 }
-                if (marr[i][j - 1] == 'O') /*mid left*/
+                if (arr[i][j - 1] == 'O') /*mid left*/
                 {
                     bb++;
                 }
-                if (marr[i][j + 1] == 'O') /*mid right*/
+                if (arr[i][j + 1] == 'O') /*mid right*/
                 {
                     bb++;
                 }
-                if (marr[i + 1][j - 1] == 'O') /*bot left*/
+                if (arr[i + 1][j - 1] == 'O') /*bot left*/
                 {
                     bb++;
                 }
-                if (marr[i + 1][j] == 'O') /*bot center*/
+                if (arr[i + 1][j] == 'O') /*bot center*/
                 {
                     bb++;
                 }
-                if (marr[i + 1][j + 1] == 'O') /*bot right*/
+                if (arr[i + 1][j + 1] == 'O') /*bot right*/
                 {
                     bb++;
                 }
                 /*if around ' ' 3 bb -> 'O'*/
-                if (marr[i][j] == sch)
+                if (arr[i][j] == sch)
                 {
+                    change(arr, &prty, orr, err);
                     if (bb == 3)
                     {
-                        bfarr[i][j] = 'O';
+                        arr[i][j] = 'O';
                     }
                     else
                     {
-                        bfarr[i][j] = sch;
+                        arr[i][j] = sch;
                     }
+                    change(arr, &prty, orr, err);
                 }
                 /*if around 'O' 2 or 3 bb -> 'O' else ' '*/
-                else if (marr[i][j] == 'O')
+                else if (arr[i][j] == 'O')
                 {
+                    change(arr, &prty, orr, err);
                     if (bb == 2 || bb == 3)
                     {
-                        bfarr[i][j] = 'O';
+                        arr[i][j] = 'O';
                     }
                     if (bb < 2 || bb > 3)
                     {
-                        bfarr[i][j] = sch;
+                        arr[i][j] = sch;
                     }
+                    change(arr, &prty, orr, err);
                 }
                 bb = 0;
             }
         }
-        /*copying data from buffer to main array*/
-        for (int i = 1; i < yarrsz - 1; i++)
-        {
-            for (int j = 1; j < xarrsz - 1; j++)
-            {
-                marr[i][j] = bfarr[i][j];
-            }
-        }
+        change(arr, &prty, orr, err);
     } while ((c = getch()) != 27 && c == 32);
     endwin();
 
